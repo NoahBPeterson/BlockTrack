40bf88dba6a5fedf4bc1b13c9c612a272a736077
noahb <noahbpeterson1997@gmail.com>
2020-06-11 02:55:54 -0500

    Add support for tracking bucket fill and empty events.

--- src/main/java/com/nbpe/listeners/BreakListener.java
+++ src/main/java/com/nbpe/listeners/BreakListener.java
@@ -1,15 +1,19 @@
 package com.nbpe.listeners;
 
-import java.util.UUID;
-
 import com.nbpe.blocktrack.BlockTrack;
 import com.nbpe.db.BlockTable;
 import com.nbpe.db.DBAccess;
 
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockLava;
+import cn.nukkit.block.BlockWater;
+import cn.nukkit.event.Event;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.player.PlayerBucketFillEvent;
 
 public class BreakListener implements Listener {
 	
@@ -18,36 +22,65 @@ public class BreakListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
 	public void breakHandler(BlockBreakEvent e)
 	{
-		UUID player = e.getPlayer().getUniqueId();
+    	breakHandler(e, e.getPlayer(), e.getBlock());
+	}
+    
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void bucketFill(PlayerBucketFillEvent e)
+    {
+    	Block takenLiquid;
+    	
+    	if(e.getItem().getDamage() == 8) //getDamage() => Meta
+    	{
+    		takenLiquid = new BlockWater();
+    		takenLiquid.x = e.getBlockClicked().x - e.getBlockFace().getUnitVector().x;
+    		takenLiquid.y = e.getBlockClicked().y - e.getBlockFace().getUnitVector().y;
+    		takenLiquid.z = e.getBlockClicked().z - e.getBlockFace().getUnitVector().z;
+    		takenLiquid.level = e.getBlockClicked().level;
+
+    	} else if(e.getItem().getDamage() == 10) //getDamage() => Meta
+    	{
+    		takenLiquid = new BlockLava();
+    		takenLiquid.x = e.getBlockClicked().x - e.getBlockFace().getUnitVector().x;
+    		takenLiquid.y = e.getBlockClicked().y - e.getBlockFace().getUnitVector().y;
+    		takenLiquid.z = e.getBlockClicked().z - e.getBlockFace().getUnitVector().z;
+    		takenLiquid.level = e.getBlockClicked().level;
+    	}else
+    	{
+    		return;
+    	}
+    	breakHandler(e, e.getPlayer(), takenLiquid);
+    }
+    
+    void breakHandler(Event event, Player player, Block block)
+    {
+    	if(block.getId() == 0) return;
+
 		dbAccess = DBAccess.getDB();
-		if(BlockTrack.playersHistoryCheck.contains(player))
+		if(BlockTrack.playersHistoryCheck.contains(player.getUniqueId()))
 		{
-			BlockTrack.bhc.blockHistorySend(e.getPlayer(), e.getBlock());
-			e.setCancelled();
+			BlockTrack.bhc.blockHistorySend(player, block);
+			event.setCancelled();
 			return;
 		} else {
-			DBAccess.blockHistoryAddEntry(player, e.getBlock(), false); //BlockHistory Tracker
-		}
-		
-		int blockType = e.getBlock().getId();
-		if(blockType == 0) return;
-		
+			DBAccess.blockHistoryAddEntry(player.getUniqueId(), block, false); //BlockHistory Tracker
+		}		
 				
-		BlockTable entry = DBAccess.getByUUIDandBlockType(player, blockType);	//Get DAO from database with the above UUID and blocktype
+		BlockTable entry = DBAccess.getByUUIDandBlockType(player.getUniqueId(), block.getId());	//Get DAO from database with the above UUID and blocktype
 		int broken = 1;
 		if(entry != null)
 		{
 			broken += entry.getDestroyed(); //Get number of {blockType} broken by player, increment
 		}else
 		{
-			DBAccess.blockTrackAddEntry(player, blockType);
-			entry = DBAccess.getByUUIDandBlockType(player, blockType);
+			DBAccess.blockTrackAddEntry(player.getUniqueId(), block.getId());
+			entry = DBAccess.getByUUIDandBlockType(player.getUniqueId(), block.getId());
 			entry.setDestroyed(broken);
 			dbAccess.blockTrackUpdateEntry(entry);
 			return;
 		}
 		entry.setDestroyed(broken); //Set number of {blockType} broken by player
 		dbAccess.blockTrackUpdateEntry(entry);
-	}
-
+    }
 }
--- src/main/java/com/nbpe/listeners/BuildListener.java
+++ src/main/java/com/nbpe/listeners/BuildListener.java
@@ -1,15 +1,19 @@
 package com.nbpe.listeners;
 
-import java.util.UUID;
-
 import com.nbpe.blocktrack.BlockTrack;
 import com.nbpe.db.BlockTable;
 import com.nbpe.db.DBAccess;
 
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockLava;
+import cn.nukkit.block.BlockWater;
+import cn.nukkit.event.Event;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockPlaceEvent;
+import cn.nukkit.event.player.PlayerBucketEmptyEvent;
 
 public class BuildListener implements Listener {
 	
@@ -18,35 +22,61 @@ public class BuildListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
 	public void buildHandler(BlockPlaceEvent e)
 	{
-		UUID player = e.getPlayer().getUniqueId();
+    	buildHandler(e, e.getPlayer(), e.getBlock());
+	}
+    
+    @EventHandler(priority = EventPriority.HIGH)
+    public void bucketEmpty(PlayerBucketEmptyEvent e)
+    {
+    	Block placedLiquid;
+    	if(e.getBucket().getDamage() == 8) //getDamage() => Meta
+    	{
+    		placedLiquid = new BlockWater();
+    		placedLiquid.x = e.getBlockClicked().x;
+    		placedLiquid.y = e.getBlockClicked().y;
+    		placedLiquid.z = e.getBlockClicked().z;
+    	} else if(e.getBucket().getDamage() == 10) //getDamage() => Meta
+    	{
+    		placedLiquid = new BlockLava();
+    		placedLiquid.x = e.getBlockClicked().x;
+    		placedLiquid.y = e.getBlockClicked().y;
+    		placedLiquid.z = e.getBlockClicked().z;
+    	}else
+    	{
+    		return;
+    	}
+    	placedLiquid.level = e.getBlockClicked().level;
+    	buildHandler(e, e.getPlayer(), placedLiquid);
+    }
+    
+    void buildHandler(Event event, Player player, Block block)
+    {
+    	if(block.getId() == 0) return;
+    	
 		dbAccess = DBAccess.getDB();
-		if(BlockTrack.playersHistoryCheck.contains(player))
+		if(BlockTrack.playersHistoryCheck.contains(player.getUniqueId()))
 		{			
-			BlockTrack.bhc.blockHistorySend(e.getPlayer(), e.getBlock());
-			e.setCancelled();
+			BlockTrack.bhc.blockHistorySend(player, block);
+			event.setCancelled();
 			return;
 		} else {
-			DBAccess.blockHistoryAddEntry(player, e.getBlock(), true); //BlockHistory Tracker
+			DBAccess.blockHistoryAddEntry(player.getUniqueId(), block, true); //BlockHistory Tracker
 		}
-
-		int blockType = e.getBlock().getId();
-		if(blockType == 0) return;
-
-				
-		BlockTable entry = DBAccess.getByUUIDandBlockType(player, blockType);	//Get DAO from database with the above UUID and blocktype
+	
+		BlockTable entry = DBAccess.getByUUIDandBlockType(player.getUniqueId(), block.getId());	//Get DAO from database with the above UUID and blocktype
 		int placed = 1;
 		if(entry != null)
 		{
 			placed += entry.getPlaced(); //Get number of {blockType} placed by player, increment
 		}else
 		{
-			DBAccess.blockTrackAddEntry(player, blockType);
-			entry = DBAccess.getByUUIDandBlockType(player, blockType);
+			DBAccess.blockTrackAddEntry(player.getUniqueId(), block.getId());
+			entry = DBAccess.getByUUIDandBlockType(player.getUniqueId(), block.getId());
 			entry.setPlaced(placed);
 			dbAccess.blockTrackUpdateEntry(entry);
 			return;
 		}
 		entry.setPlaced(placed); //Set number of {blockType} placed by player
 		dbAccess.blockTrackUpdateEntry(entry);
-	}
+    }
 }
